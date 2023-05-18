f36768f954a4fbbb56a0dcca1e48220814fda4e8
DDimbe <109931951+DmytroDatsiuk@users.noreply.github.com>
2023-05-18 15:37:33 +0300

    deploy server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+//* g9X$J5M5C.Ry6Bx
+
+const DB_HOST =
+  "mongodb+srv://Dmytro:g9X$J5M5C.Ry6Bx@dimbedb.azhtf6n.mongodb.net/my_contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(app.listen(3000))
+  .catch((e) => {
+    console.log(e.message);
+    process.exit(1);
+  });
